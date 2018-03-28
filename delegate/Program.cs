fa1155182ecfeb7eefdc81de62db2ba2ac57ea8b
Coldstream-Louis <dushuyang@126.com>
2018-03-28 21:06:26 +0800

    initial

---
+++ delegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delegate
+{
+    public delegate void AnsweringDelegate(string question);
+    
+    class Program
+    {
+        private static void AnswerName(string question) {
+            Console.WriteLine("Your name is "+question);
+        }
+        private static void AnswerBirthday(string question) {
+            Console.WriteLine("Your birthday is "+question);
+        }
+        
+        private static void AnswerTime(string question) {
+            Console.WriteLine("Now is "+System.DateTime.Now);
+        }
+        private static void WhatCanIDo(string question) {
+            Console.WriteLine("Hi, I am Robort Zero, I can answer your name, your birthday and today's date, now I will show you my capabilities.");
+        }
+        
+        private static void GiveMeAnswer(string question, AnsweringDelegate MakeAnswering) {
+            MakeAnswering(question);
+        }
+
+        static void Main(string[] args) {
+            GiveMeAnswer("What can you do?", WhatCanIDo);
+            GiveMeAnswer("Edward Plantagenet", AnswerName);
+            GiveMeAnswer("1988.6.7", AnswerBirthday);
+            GiveMeAnswer("What's the time?", AnswerTime);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ interface/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    public interface IGreeting {
+        void GreetingPeople(string name);
+    }
+
+    public class Python3Greeting : IGreeting {
+        public void GreetingPeople(string name) {
+            Console.WriteLine("print('Hello, "+name+"!')");
+        }
+    }
+    public class CppGreeting : IGreeting {
+        public void GreetingPeople(string name) {
+            Console.WriteLine("cout<<'Hello, "+name+"!'<<endl;");
+        }
+    }
+    public class JavaGreeting : IGreeting {
+        public void GreetingPeople(string name) {
+            Console.WriteLine("System.out.println('Hello, "+name+"!');");
+        }
+    }
+    public class CSharpGreeting : IGreeting {
+        public void GreetingPeople(string name) {
+            Console.WriteLine("Console.WriteLine('Hello, "+name+"!');");
+        }
+    }
+    
+    class Program
+    {
+        private static void GreetPeople(string name, IGreeting MakeGreeting) {
+            MakeGreeting.GreetingPeople(name);
+        }
+
+        static void Main(string[] args) {
+            GreetPeople("Jessica", new Python3Greeting());
+            GreetPeople("Francis", new CppGreeting());
+            GreetPeople("Alice", new JavaGreeting());
+            GreetPeople("Katherine", new CSharpGreeting());
+            Console.ReadKey();
+        }
+    }
+}
